5bcb3bdb9d5fbc6bbdf23c89ead781bec847b9fa
towhid046 <towhidmorol46@gmail.com>
2024-03-12 06:33:45 +0600

    set functionality to add bottols to the cart

--- src/components/Bottol/Bottol.jsx
+++ src/components/Bottol/Bottol.jsx
@@ -1,18 +1,20 @@
 import  PropTypes  from "prop-types"
 import './Bottol.css'
-const Bottol = ({bottol}) => {
+const Bottol = ({bottol, handelAddToCart}) => {
 const {name, img, price} = bottol;
     return (
         <div className="bottol">
             <h2>{name}</h2> 
             <img src={img} alt="Bottol Image" />
             <h5>Price: ${price}</h5>
+            <button onClick={()=> handelAddToCart(bottol)} >Add to cart</button>
         </div>
     );
 };
 
 Bottol.propTypes = {
-    bottol: PropTypes.array.isRequired
+    bottol: PropTypes.object.isRequired,
+    handelAddToCart: PropTypes.func,
 }
 
 export default Bottol;
--- src/components/Bottols/Bottols.jsx
+++ src/components/Bottols/Bottols.jsx
@@ -4,6 +4,7 @@ import './Bottols.css'
 
 const Bottols = () => {
   const [bottols, setBottols] = useState([]);
+  const [cartBottols, setCartBottols] = useState([]);
 
   useEffect(() => {
     const loadData = async () => {
@@ -14,13 +15,23 @@ const Bottols = () => {
     loadData();
   }, []);
 
+
+  const handelAddToCart = (bottol) => {
+    setCartBottols([...cartBottols, bottol])
+  }
+
   return (
     <>
-      <h2>Memorable water bottols</h2>
+      <h2>Memorable water bottols: {bottols.length}</h2>
+      <h3>Cart: {cartBottols.length}</h3>
 
       <div className="bottol-container">
         {bottols.map((bottol) => (
-          <Bottol key={bottol.id} bottol={bottol} />
+          <Bottol 
+          key={bottol.id} 
+          bottol={bottol}
+          handelAddToCart={handelAddToCart}
+           />
         ))}
       </div>
     </>
